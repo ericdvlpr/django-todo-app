220c85f6115ab1a27c5f0ea5e65c6fdaaca7b0d5
Eric Jaucian <gitemail@shoperones.com>
2020-03-31 16:16:50 +0800

    live files added

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-todo-crud-app.herokuapp.com','127.0.0.1']
 
 
 # Application definition
